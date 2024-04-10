67ae444736169e0f3c2bfc6392d3a13d0d1b1453
Collins Muriuki <hello@collinsmuriuki.xyz>
2024-04-10 14:34:13 +0300

    refactor types

--- ui/src/types.ts
+++ ui/src/types.ts
@@ -2,9 +2,10 @@ import { ActionCommittedSignal } from "@holochain-open-dev/utils";
 
 export type ProfilesSignal = ActionCommittedSignal<EntryTypes, any>;
 
-export type EntryTypes = { type: "Profile" } & Profile;
+export type EntryTypes = Profile;
 
 export interface Profile {
+  type: "Profile";
   nickname: string;
   fields: Record<string, string>;
 }
